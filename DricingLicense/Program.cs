635125df2cabea932a2d450ccb8dd502d586e868
Carmel Meelind <cmeelind@tktk.ee>
2022-03-24 14:43:12 +0200

    Control Flow

---
+++ DricingLicense/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace DricingLicense
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib kasutaja sünniaastat;
+            //programm arvutab kasutaja vanuse;
+            //kui kasutaja on noorem kui 18, siis programm kuvab konsoolis 
+            //"oled liiga noor, et juhilube saada";
+            //kui kasutaja on vanem kui 18, siis programm kuvab konsoolis
+            //"oled piisavalt vana, et juhilube saada";
+            //kui kasutaja on 18, siis programm kuvab konsoolis
+            //"Palju õnne! Nüüd sa saad juhiluba taotleda";
+
+            Console.WriteLine("Palun sisesta oma sünniaasta.");
+            int yearOfBirth = Convert.ToInt32(Console.ReadLine());
+
+            if(yearOfBirth < 2004)
+            {
+                Console.WriteLine("Oled piisavalt vana, et juhilube saada.");
+            }
+            else if(yearOfBirth > 2004)
+            {
+                Console.WriteLine("Oled liiga noor, et juhilube saada.");
+            }
+            else
+            {
+                Console.WriteLine("Palju õnne! Nüüd sa saad juhilube taotleda.");
+            }
+
+            Console.WriteLine("Kena päeva!");
+        }
+    }
+}
